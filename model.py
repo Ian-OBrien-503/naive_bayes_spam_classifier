64d8ecd79b3097c2056430ec8892098cc07c6b5c
Your Name <iobrien@pdx.edu>
2019-05-21 18:17:06 -0700

    issue with taking log, may be possibile underflow, need to see where domain error is coming from

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import statistics as stat
+from math import e, log, sqrt, pi
 
 
 # this function gets all of the data into a data matrix and then splits the data into
@@ -33,8 +34,8 @@ def check_data(test_matrix, train_matrix):
             not_spam = not_spam + 1
         else:
             spam = spam + 1
-    test_prior_spam = spam / 2301
-    test_prior_not_spam = not_spam /2301
+    test_prior_spam = spam / 2300
+    test_prior_not_spam = not_spam / 2300
     print("spam prior test set =", test_prior_spam)
     print("NOT spam prior test set =", test_prior_not_spam)
     # check ratio for training set
@@ -45,33 +46,128 @@ def check_data(test_matrix, train_matrix):
             not_spam = not_spam + 1
         else:
             spam = spam + 1
-    train_prior_spam = spam / 2300
-    train_prior_not_spam = not_spam / 2300
-    print("spam prior test set =", train_prior_spam)
-    print("NOT spam prior test set =", train_prior_not_spam)
-    mean_and_std_dev(train_matrix)
+    train_prior_spam = spam / 2301
+    train_prior_not_spam = not_spam / 2301
+    print("spam prior training set =", train_prior_spam)
+    print("NOT spam prior training set =", train_prior_not_spam)
+    mean_and_std_dev(train_matrix, test_matrix, train_prior_spam, train_prior_not_spam, spam, not_spam)
 
 
 # compute the mean and standard deviation for each feature in the training set given each class
-def mean_and_std_dev(train_matrix):
-    min = 0.0001
-    for i in range(1, 57):
-        mean_feature_x = stat.mean(train_matrix[:, i])
-        std_feature_x = stat.stdev(train_matrix[:, i])
-        if mean_feature_x == 0.0:
+def mean_and_std_dev(train_matrix, test_matrix, train_prior_spam, train_prior_not_spam, spam, not_spam):
+    train_spam = np.empty((spam, 58))
+    train_not_spam = np.empty((not_spam, 58))
+    i = 0
+    j = 0
+    spam_m = []
+    spam_s = []
+    notSpam_m = []
+    notSpam_s = []
+
+    # break training array into two subarrays that are catagorized by class
+    for row in range(0, 2300):
+        if train_matrix[row][57] == 0:
+            train_not_spam[i] = train_matrix[row]
+            i = i + 1
+        else:
+            train_spam[j] = train_matrix[row]
+            j = j+1
+    # calculate stdev, mean for both classes
+    for i in range(0, 57):
+        spam_mean_feature_x = stat.mean(train_spam[:, i])
+        spam_m.append(spam_mean_feature_x)
+        spam_std_feature_x = stat.stdev(train_spam[:, i])
+        spam_s.append(spam_std_feature_x)
+        not_spam_mean_feature_x = stat.mean(train_not_spam[:, i])
+        notSpam_m.append(not_spam_mean_feature_x)
+        not_spam_std_feature_x = stat.stdev(train_not_spam[:, i])
+        notSpam_s.append(not_spam_std_feature_x)
+        if spam_m[i] == 0.0:
+            spam_m[i] = 0.0001
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            break
-        if std_feature_x == 0.0:
+        if spam_s[i] == 0.0:
+            spam_s[i] = 0.0001
             print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            break
-        print("mean feature #", i, ":", mean_feature_x)
-        print("stdev feature #", i, ":", std_feature_x)
-        naive_bayes(mean_feature_x, std_feature_x)
+        if notSpam_m[i] == 0.0:
+            notSpam_m[i] = 0.0001
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        if notSpam_s == 0.0:
+            notSpam_s = 0.0001
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    naive_bayes(test_matrix, notSpam_s, notSpam_m, spam_s, spam_m, train_spam, train_not_spam, train_prior_spam, train_prior_not_spam)
 
 
 # this is where we do the niave bayes classification
-def naive_bayes(mean, stdev):
-    
+def naive_bayes(test_matrix, notSpam_s, notSpam_m, spam_s, spam_m, train_spam, train_not_spam, train_prior_spam, train_prior_not_spam):
+    cond_prob_notspam = 0
+    cond_prob_spam = 0
+    conf_predict = []
+    conf_true = test_matrix[:, 57]
+
+
+    # calculate cond probability of feature given class spam
+    q = 0
+    r = 0
+    for i in range(0, 2300):
+        cond_prob_notspam = log(train_prior_not_spam)
+        cond_prob_spam = log(train_prior_spam)
+        for j in range(0, 56):
+            x = pow(e, -1 * ( ((test_matrix[i][j] - spam_m[j])**2) / ((2*spam_s[j])**2) ) )
+            x = x / (sqrt(2*pi)*spam_s[j])
+            # x = log(x)
+            cond_prob_spam = cond_prob_spam + x
+    # calculate cond probability of feature given class NOTSPAM
+            z = pow(e, -1 * ( ((test_matrix[i][j] - notSpam_m[j])**2) / ((2*notSpam_s[j])**2) ))
+            z = z / (sqrt(2*pi)*spam_s[j])
+            # z = log(z)
+            cond_prob_notspam = cond_prob_notspam + z
+
+        if cond_prob_notspam > cond_prob_spam:
+            q = q + 1
+            conf_predict.append(0)
+        else:
+            r = r + 1
+            conf_predict.append(1)
+    print(q, "NOT SPAM PREDICTIONS")
+    print(r, "SPAM PREDICTIONS")
+    confustion_matrix = np.zeros((2, 2))
+
+
+    # build confusion matrix and do some calculations
+    p = 0
+    n = 0
+    # build confusion matrix
+    for i in range(0, 2300):
+        if conf_true[i] == 1:
+            p = p + 1
+    n = (2300 - p)
+
+    tn = 0
+    tp = 0
+    fp = 0
+    fn = 0
+    for i in range(0, 2300):
+            if conf_predict[i] == conf_true[i] and conf_predict[i] == 1 and conf_true[i] == 1:
+                tp = tp + 1
+            if conf_predict[i] == conf_true[i] and conf_predict[i] == 0 and conf_true[i] == 0:
+                tn = tn + 1
+            if conf_predict[i] != conf_true[i] and conf_predict[i] == 0 and conf_true[i] == 1:
+                fn = fn + 1
+            if conf_predict[i] != conf_true[i] and conf_predict[i] == 1 and conf_true[i] == 0:
+                fp = fp + 1
+
+    # these calculations are in accordance with pg. 23 of marsland text
+    accuracy = (tp + fp) / (tp + fp + tn + fn)
+    print("accuracy of model ~", accuracy)
+    precision = tp / (p + fn)
+    print("precision of model ~", precision)
+    recall = tp / (tp + fn)
+    print("recall of model ~", recall)
+
+
+
+
+
 
 # main program
 load_data()
